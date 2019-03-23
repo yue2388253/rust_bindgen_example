e12c1ea90507b88aee317e77d566cfe32f008b0c
Lam <15622383059@163.com>
2019-03-23 10:42:04 +0800

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ unsafe fn call_hello_from_c(){
 
 
 fn main() {
-    println!("lib_path is [{}]", env::current_dir().unwrap().join("build").display());
     println!("Hello from Rust.");
     unsafe {
         call_hello_from_c();
